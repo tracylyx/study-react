ecef9a19c88d6d0338c24f0df73f2be2fc372820
tracylyx <tracylyx1992@gmail.com>
2023-04-26 08:08:48 +0800

    修改配置1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// step2 修改react和react-dom的引用方式
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
